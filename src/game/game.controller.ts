de4b1cd131472149582a2a7161385fb6a61cee3f
lauta <altairdiaz245@gmail.com>
2024-01-11 20:05:57 -0300

    md

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,20 +1,43 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Param, Query } from "@nestjs/common";
 import { Games } from "src/entity/Games.entity";
 import { GameService } from "./game.service";
 
-@Controller("game")
+@Controller("games")
 export class GameController {
   constructor(private readonly appService: GameService) { }
 
+  // Obtener todos loe juegos
   @Get()
-  async getHello(): Promise<Games[]> {
-    return await this.appService.getHello();
+  async getAllGames(): Promise<Games[]> {
+    return await this.appService.getAllGames();
   }
 
-  @Post("/asd")
-  async i(
-    @Body() body: { title: string }
+  // Obtener los juegos mediante su id
+  @Get("id/:id") // Ejemplo: game/id/1
+  async getGameById(
+    @Param() param: { id: string }
   ) {
-    return await this.appService.insertData(body.title);
+    const idToNumber = parseInt(param.id)
+    return await this.appService.getGameById(idToNumber)
   }
+
+  // Obtener los juegos mediante su consola
+  @Get("console/:consoleVideoGame") // Ejemplo: game/console/ps1
+  async getGameByConsole(
+    @Param() param: { consoleVideoGame: string }
+  ) {
+    const { consoleVideoGame } = param
+    return await this.appService.getGameByConsole(consoleVideoGame)
+  }
+
+  // Obtener los juegos mediante su generación
+  @Get("genration/:generation") // Ejemplo -> generation/6
+  async getGameByGeneration(
+    @Param() param: { generation: string }
+  ) {
+    const generationToNumber = parseInt(param.generation)
+    return await this.appService.getGameByGeneration(generationToNumber)
+  }
+
+
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Games } from 'src/entity/Games.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, FindOneOptions, FindOptionsWhere, Repository } from 'typeorm';
+
 
 @Injectable()
 export class GameService {
@@ -10,15 +11,93 @@ export class GameService {
     private GamesRepository: Repository<Games>
   ) { }
 
-  async getHello(): Promise<Games[]> {
-    return await this.GamesRepository.find()
+  private async findOneBy({ ...data }: FindOptionsWhere<Games>) {
+    const opts: FindOneOptions<Games> = {
+      where: { ...data }
+    }
+
+    try {
+      const game = await this.GamesRepository.find(opts)
+      if (!game.length) {
+        return {
+          message: `Esta/e ${Object.keys(data)[0]} no existe`,
+          status: HttpStatus.NOT_FOUND,
+          error: null,
+          game
+        }
+      }
+
+      return game
+    } catch (error) {
+      return {
+        message: "Internal Server Error",
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: true
+      }
+    }
+
   }
 
+  async getGameById(id: number): Promise<Games[] | {
+    message: string;
+    status: number;
+    error: null | boolean;
+  }> {
+
+    try {
+      const game = await this.findOneBy({ id })
+      return game
+    } catch (e) {
+      console.log(e)
+    }
+
+  }
+
+  async getGameByConsole(console: string): Promise<Games[] | {
+    message: string;
+    status: number;
+    error: null | boolean;
+  }> {
+    const game = await this.findOneBy({ consoleSmallName: console })
+    return game
+  }
+
+  async getGameByGeneration(generation: number): Promise<Games[] | {
+    message: string;
+    status: number;
+    error: null | boolean;
+  }> {
+    const game = await this.findOneBy({ generation })
+    return game
+  }
+
+  async getAllGames(): Promise<Games[]> {
+    const opts: FindManyOptions<Games> = {}
+
+    return await this.GamesRepository.find(opts)
+  }
+
+  async q(q: { limit: number | undefined, console: string | undefined }) {
+    const { console, limit } = q
+    const consoleToUpperCase = console?.toUpperCase()
+
+    const opts: FindManyOptions<Games> = {
+      take: limit || 50,
+      where: consoleToUpperCase ? { consoleSmallName: consoleToUpperCase } : {},
+      cache: true
+    }
+
+    return await this.GamesRepository.find(opts)
+  }
+
+  /*
   async insertData(title: string) {
     const game = this.GamesRepository.create({
       title
     })
     return await this.GamesRepository.insert(game)
   }
+
+  */
 
 }
