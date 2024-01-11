4da2791c1731538c7f851e1c9d2470ecd3b39198
lauta <altairdiaz245@gmail.com>
2024-01-11 20:03:54 -0300

    Crear una tabla para agregar los juegos

---
+++ createTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    descripcion TEXT,
+    genre TEXT,
+    console_public_name TEXT,
+    console_small_name TEXT,
+    generation INTEGER,
+    release_year INTEGER,
+    precio REAL,
+    cover TEXT
+);
