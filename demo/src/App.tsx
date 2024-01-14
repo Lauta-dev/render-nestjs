0c10301eb5222a31995d607c116d69711ccefbde
lauta <altairdiaz245@gmail.com>
2024-01-14 13:02:12 -0300

    Elminado el context

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,15 +3,12 @@ import Home from "./components/Home";
 import GameInfo from "./components/game-info";
 import Header from "./components/Header";
 import GameConsoleInfo from "./components/game-console-list";
-import { CharacterProvider } from "./context/ChangeHome";
 
 const App = () => {
 	return (
 		<>
-			<CharacterProvider>
-				<Header />
-				<Route path="/" component={Home} />
-			</CharacterProvider>
+			<Header />
+			<Route path="/" component={Home} />
 
 			<Route path="/game/:id" component={GameInfo} />
 			<Route path="/item/:selected/:i" component={GameConsoleInfo} />
