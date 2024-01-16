661d585c289dd06f0f7a46519a2da30ad10395c8
lauta <altairdiaz245@gmail.com>
2024-01-16 00:30:01 -0300

    Eliminado el test

--- src/middleware/generation-validation/generation-validation.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GenerationValidationMiddleware } from './generation-validation.middleware';
-
-describe('GenerationValidationMiddleware', () => {
-  it('should be defined', () => {
-    expect(new GenerationValidationMiddleware()).toBeDefined();
-  });
-});
