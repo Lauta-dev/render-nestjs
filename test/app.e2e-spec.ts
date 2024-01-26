845321ae08b0609a03afa21e36e9576c07a56829
lauta <altairdiaz245@gmail.com>
2024-01-26 16:10:43 -0300

    Test end 2 end

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication } from "@nestjs/common";
+import { moduleRef, req } from "./helper";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("App", () => {
+	let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+	beforeAll(async () => {
+		const ref = await moduleRef();
+		app = ref.createNestApplication();
+		await app.init();
+	});
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+	afterAll(async () => {
+		app.close();
+	});
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+	it("/GET consoles", () => {
+		return req(app, "/consoles").expect(200);
+	});
 });
