b4b39a810877453369650ee88c740316ebaee04f
lauta <altairdiaz245@gmail.com>
2024-01-26 16:11:17 -0300

    Mock para el test unitario

---
+++ src/utils/allConsoles.ts
@@ -0,0 +1,9 @@
+export const allConsoles = [
+	{ short: "ps1", public: "PlayStation 1" },
+	{ short: "ps2", public: "PlayStation 2" },
+	{ short: "ps3", public: "PlayStation 3" },
+	{ short: "ps4", public: "PlayStation 4" },
+	{ short: "xbox", public: "Xbox" },
+	{ short: "xbox_360", public: "Xbox 360" },
+	{ short: "xbox_one", public: "Xbox One" },
+];
